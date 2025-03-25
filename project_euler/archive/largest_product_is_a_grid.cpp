95055f4991a741eb9b6ee671a82613e253302779
A-Shermatov <azamatjon.shermatov@gmail.com>
2025-03-26 00:19:49 +0300

    Added template for codeforces contests

---
+++ project_euler/archive/largest_product_is_a_grid.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+
+void solve() {
+
+}
+
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    solve();
+}
